0cdbfe25b6f721fddd4dcd5cf0128256145bf63d
AbelardoCondoriFernandez <abelardocf0110@gmail.com>
2021-07-24 09:01:36 -0500

    update tokens to 3000 seconds

--- src/main/java/com/clinica/AuthorizationServerConfig.java
+++ src/main/java/com/clinica/AuthorizationServerConfig.java
@@ -49,7 +49,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
 		configurer.inMemory().withClient(clientId).secret(clientSecret).authorizedGrantTypes(grantType)
-		        .scopes(scopeRead, scopeWrite).resourceIds(resourceIds).accessTokenValiditySeconds(300)
+		        .scopes(scopeRead, scopeWrite).resourceIds(resourceIds).accessTokenValiditySeconds(3000)
 		        .refreshTokenValiditySeconds(0);
 	}
 //genera una cadena de string
